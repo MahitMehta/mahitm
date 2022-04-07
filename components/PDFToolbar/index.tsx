e09230ea8fc76229a6f38c6a14bf419b7ee28133
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-604.local>
2022-04-07 10:13:32 -0400

    Created Resume API Route

--- components/PDFToolbar/index.tsx
+++ components/PDFToolbar/index.tsx
@@ -1,14 +1,11 @@
 import { faFileDownload, faPrint, faExpand } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import React, { useMemo } from "react";
+import React from "react";
 import SocialButton from "../SocialButton";
 
 const RESUME_URL_ORIGINAL = "https://res.cloudinary.com/mahitm-cdn/image/upload/mahitm/resume.pdf";
 
 const PDFToolbar = () => {
-    const router = useRouter();
-
     const handlePrint = (e:React.MouseEvent<HTMLLIElement>) => {
         const printJS = require("print-js");
 
@@ -17,57 +14,10 @@ const PDFToolbar = () => {
 
         printJS({ printable: RESUME_URL_ORIGINAL, type: "pdf"});
     }
- 
-    const iosDevice = useMemo(() => {
-        const userAgent = navigator.userAgent || navigator.vendor || (window as any)?.opera; 
-        return /iPad|iPhone|iPod/i.test(userAgent) && !(window as any)?.MSStream;
-    }, []);
 
-
-    const handleDownload = async (e:React.MouseEvent<HTMLLIElement>) => {
-        e.preventDefault();
-        e.stopPropagation();
-    
-        
-
-        const file = await fetch(RESUME_URL_ORIGINAL);
-        const data = await file.arrayBuffer();
-     
-        const blob = new Blob([ data ], { type: "application/pdf" });
-
-        let blobURL:string; 
-
-        if (iosDevice) {
-            return;
-            // const reader = new FileReader();
-        
-            // reader.readAsDataURL(blob); 
-            // const dataURL:string | null = await new Promise((resolve, _reject) => {
-                
-            //     reader.onload = (e) => {
-            //         !!reader.result ? resolve(reader.result.toString()) : resolve(null);
-            //     }
-            // });
-
-            // blobURL = dataURL ?? URL.createObjectURL(blob);
-        } else {
-            blobURL = URL.createObjectURL(blob);
-        }
-
-        const resumeLink = document.createElement('a');
-        resumeLink.href = blobURL;
-        resumeLink.download = "resume.pdf";
-
-        resumeLink.dispatchEvent(new MouseEvent('click', { 
-            bubbles: true, 
-            cancelable: true, 
-            view: window,
-        }));
-    };
-    
     return (
         <ul className="flex">
-            <Link href={RESUME_URL_ORIGINAL} passHref>
+            <Link href={"/api/resume"} passHref>
                <a download>
                     <li title="Download" style={{ listStyle: "none" }}>
                         <SocialButton icon={faFileDownload}/>
---
+++ pages/api/resume.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import fetch from "node-fetch";
+import stream from 'stream';
+import { promisify } from 'util';
+
+const pipeline = promisify(stream.pipeline);
+
+const RESUME_URL_ORIGINAL = "https://res.cloudinary.com/mahitm-cdn/image/upload/mahitm/resume.pdf"; 
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+
+  const response = await fetch(RESUME_URL_ORIGINAL).catch(() => null);
+  if (!response) {
+      res.status(500).send(null);
+      return
+  }
+
+  res.setHeader('Content-Type', 'application/pdf');
+  res.setHeader('Content-Disposition', 'attachment; filename=resume.pdf');
+
+  return await pipeline(response.body as any, res);
+}
